21ed961a40708a4763d4ab4751424716ae21f54f
Paulo Xavier <px568661@gmail.com>
2026-04-10 00:02:42 -0300

    :sparkles: First commit - Implemented register of users and ages. The list operation is working too.

---
+++ Project_User_Registration/README.md
@@ -0,0 +1,28 @@
+1. Sistema de Cadastro de Usuários (console)
+
+O que praticar:
+
+Vetores (armazenar usuários)
+Funções (cadastrar, listar, buscar)
+Estruturas de repetição (menu)
+Condições (validação)
+
+Desafio:
+
+Criar um menu tipo:
+
+2 - Listar usuários
+3 - Buscar usuário
+4 - Sair
+
+Armazenar nome e idade em arrays
+Limitar quantidade (ex: 100 usuários)
+
+
+
+
+
+Upgrade (nível +):
+
+Evitar usuários duplicados
+Permitir remoção
---
+++ Project_User_Registration/UserRegistration.java
@@ -0,0 +1,94 @@
+/*
+Desafio:
+
+Criar um menu tipo:
+
+1 - Cadastrar usuário
+2 - Listar usuários
+3 - Buscar usuário
+4 - Sair
+
+Armazenar nome e idade em arrays
+Limitar quantidade (ex: 100 usuários)
+*/
+
+import java.util.Scanner;
+
+public class UserRegistration {
+
+    static String[] users = new String[10];
+    static int[] ages = new int[10];
+
+    static int totalUsers = 0;
+    
+    static Scanner myObj = new Scanner(System.in);
+
+    public static void menu() {
+
+        while (true) {
+
+            System.out.println("====== USERS REGISTRATION ======");
+            System.out.println("1. Register user");
+            System.out.println("2. List Users");
+            System.out.println("3. Search User");
+            System.out.println("0. Exit");
+
+            System.out.println();
+
+            int op = myObj.nextInt();
+
+            switch (op) {
+                
+                case 1: 
+                    System.out.println("Please, enter the name: ");
+                    String name = myObj.next();
+
+                    System.out.println("Enter the age...");
+                    int age = myObj.nextInt();
+
+                    boolean result = registerUserAndAge(name, age);
+
+                    if (result) {
+                        System.out.println("USER REGISTERED!!");
+                    } else {
+                        System.out.println("USER NOT REGISTERED! TRY AGAIN LATER...");
+                    }
+                    break;
+
+                case 2: 
+                    listUsersAndAge();
+                    break; 
+
+                default:
+                    System.out.println("Invalid option! Restarting the application...");
+            }
+
+        }
+    }
+
+    public static boolean registerUserAndAge(String name, int age) {
+        users[totalUsers] = name;
+        ages[totalUsers] = age;
+        totalUsers++;
+        return true; 
+    }
+
+    public static void listUsersAndAge() {
+
+        System.out.println("======== USERS ========");
+
+        for (int i = 0; i < users.length; i++) {
+
+            if (users[i] == null){
+                continue;
+            
+            } else {
+                System.out.println("Username:" + users[i] + " | Age: " + ages[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        menu();
+    }
+}

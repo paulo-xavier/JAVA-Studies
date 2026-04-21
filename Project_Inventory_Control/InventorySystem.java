db190e5592038eec406348ed2e8db33a6fe93977
Paulo Xavier <px568661@gmail.com>
2026-04-20 23:50:30 -0300

    :sparkles: Implementing product registration function

--- Project_Inventory_Control/InventorySystem.java
+++ Project_Inventory_Control/InventorySystem.java
@@ -12,19 +12,74 @@ public class InventorySystem {
 
     public static void menu() {
 
-        while(true){
+        while (true) {
 
             System.out.println("===== INVENTORY SYSTEM =====");
-            System.out.println("1. Register product"); 
-            System.out.println("2. List products"); 
-            System.out.println("3. Search product"); 
-            System.out.println("4. Product stock-in "); 
-            System.out.println("5. Product stock-out "); 
-            System.out.println("6. Almost out-of-stock products"); 
-            System.out.println("0. EXIT"); 
+            System.out.println("1. Register product");
+            System.out.println("2. List products");
+            System.out.println("3. Search product");
+            System.out.println("4. Product stock-in ");
+            System.out.println("5. Product stock-out ");
+            System.out.println("6. Almost out-of-stock products");
+            System.out.println("0. EXIT");
 
-            System.out.print("Choose an option: "); 
-            int op = Integer.parseInt(myObj.nextLine()); 
+            System.out.print("Choose an option: ");
+            int op = Integer.parseInt(myObj.nextLine());
+
+            switch (op) {
+
+                case 1:
+                    System.out.println("Enter the product name: ");
+                    String productName = myObj.nextLine();
+
+                    System.out.println("Enter the product quantity: ");
+                    int productQuantity = Integer.parseInt(myObj.nextLine());
+
+                    System.out.println("Enter the product price: ");
+                    float productPrice = Float.parseFloat(myObj.nextLine());
+
+                    boolean result = registerProduct(productName, productQuantity, productPrice);
+
+                    if (result) {
+                        System.out.println("Product Successfully registered!!");
+                    } else {
+                        System.out.println("Product not registered! TRY AGAIN...");
+                    }
+
+                    break;
+
+                case 2:
+
+            }
+        }
+    }
+
+
+    public static void listProducts() {
+        
+    }
+
+    public static boolean registerProduct(String productName, int productQuantity, float productPrice) {
+
+        try {
+            products[totalProducts] = productName;
+            quantity[totalProducts] = productQuantity;
+            price[totalProducts] = productPrice;
+            totalProducts++;
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Fail to register product!!");
+            return false;
+        }
+    }
+
+    public static boolean checkIsPossibleToRegisterProduct() {
+        if (totalProducts == 5) {
+            return false; // Not possible
+
+        } else {
+            return true; // Possible
         }
     }
 

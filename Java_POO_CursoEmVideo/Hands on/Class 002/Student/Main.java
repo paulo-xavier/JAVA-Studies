ac330f143624f29902bb2c43e64385116f60c5e0
Paulo Xavier <px568661@gmail.com>
2026-05-03 00:09:11 -0300

    📄 Curso em Video Java - Practicing Class and Object

--- Java_POO_CursoEmVideo/Hands on/Class 002/Student/Main.java
+++ Java_POO_CursoEmVideo/Hands on/Class 002/Student/Main.java
@@ -1,16 +1,46 @@
 public class Main {
-    public static void main(String[] args){
-        Student a = new Student(); 
-        
+    public static void main(String[] args) {
+        Student a = new Student();
+
         a.grades = new float[4];
 
-        a.addGrades(5); 
-        
-        a.addGrades(10); 
-        a.addGrades(15); 
-        a.addGrades(20); 
+        a.toSetName("Maria Helena Xavier"); 
+        a.toSetAge(23);
+
+        a.addGrades(5);
+        a.addGrades(20);
+        a.addGrades(15);
+        a.addGrades(20);
 
         a.toShowGrades();
-        
+
+        System.out.println("Average: " + a.toCalculateAverage());
+
+        a.toCheckApproval(a.toCalculateAverage());
+
+        System.out.println("\n=========\n");
+
+        Student b = new Student();
+
+        b.toSetName("Paulo Roberto Xavier");
+        b.toSetAge(21);
+
+        b.grades = new float[3];
+
+        b.addGrades(1);
+        b.addGrades(2);
+        b.addGrades(3);
+
+        b.toShowGrades();
+
+        System.out.println("Average: " + b.toCalculateAverage());
+
+        b.toCheckApproval(b.toCalculateAverage());
+
+        System.out.println("\n=========\n");
+
+        b.toGenerateReport();
+        a.toGenerateReport();
+
     }
 }
--- Java_POO_CursoEmVideo/Hands on/Class 002/Student/Student.java
+++ Java_POO_CursoEmVideo/Hands on/Class 002/Student/Student.java
@@ -2,24 +2,60 @@ public class Student {
     String name;
     int age;
     float[] grades;
+    int totalGrades;
     boolean isApproved;
 
     public void addGrades(float grade) {
-        for (int i = 0; i < this.grades.length; i++) {
-            if (this.grades[i] > 0) { // It means that a grade exists in this position
-                continue;
-            } else {
-                this.grades[i] = grade;
-            }
-        }
+        this.grades[totalGrades] = grade;
+        totalGrades++;
     }
 
     public void toShowGrades() {
-        
-        for (int i = 0; i < this.grades.length; i++) {
+        for (int i = 0; i < this.totalGrades; i++) {
             System.out.printf("Grade %d: %.2f \n", i + 1, this.grades[i]);
+        }
+    }
 
+    public float toCalculateAverage() {
+        int gradesSum = 0;
+        int gradesQuantity = this.totalGrades;
+
+        for (int i = 0; i < this.totalGrades; i++) {
+            gradesSum += this.grades[i];
         }
 
+        int average = gradesSum / gradesQuantity;
+
+        return average;
+    }
+
+    public void toCheckApproval(float average) {
+        if (average >= 7) {
+            System.out.println("APPROVED!");
+            this.isApproved = true; 
+        } else {
+            System.out.println("REPROVED!");
+            this.isApproved = false; 
+        }
+    }
+
+    public void toGenerateReport() {
+        System.out.println("\n==== STUDENT INFORMATION ====\n");
+        System.out.printf("| Name: %s |\n", this.name);
+        System.out.printf("| Age: %d |\n", this.age);
+
+        for (int i = 0; i < this.totalGrades; i++) {
+            System.out.printf("| Grade %d: %.2f \n", i + 1, this.grades[i]);
+        }
+
+        System.out.printf("| Is approved?  %b |", this.isApproved);
+    }
+
+    public void toSetName(String name){
+        this.name = name; 
+    }
+
+    public void toSetAge(int age){
+        this.age = age; 
     }
 }
